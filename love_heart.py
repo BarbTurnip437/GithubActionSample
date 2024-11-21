c4d97ff651111115066cc43e249e23c46f18532e
Cbscfe <125780968+BarbTurnip437@users.noreply.github.com>
2024-11-21 20:27:30 +0800

    Update love_heart.py

--- love_heart.py
+++ love_heart.py
@@ -8,16 +8,16 @@ def rename_file(old_name, new_name):
         os.rename(old_name, new_name)
         print(f"[提示] 已重命名文件至 {new_name}")
     except FileNotFoundError:
-        print(f"[错误] 文件未找到: {old_name}")
+        print(f"[错误] 文件不存在: {old_name}")
     except PermissionError:
-        print(f"[错误] 无法重命名文件，权限被拒绝: {old_name}")
+        print(f"[错误] 无法重命名文件，权限不足: {old_name}")
     except Exception as e:
         print(f"[错误] 未知错误: {e}")
 
 # Read or create the configuration file
 try:
     with open("禁用助手配置文件.txt", "x", encoding="utf-8") as f:
-        f.writelines(['\\\\\t使用说明\n', '\\\\\t"\\\\"开始为注释(为啥配置文件要注释?)\n', '\\\\\t"."开始为直接输出，最后一个会读取输入\n', '\\\\\t类似"input$file"的内容为\n', '\\\\\t"^"为同时操作的文件\n', '\n', '\n', '.禁用助手 Beta 2.1.1 - LIB临时工作室出品\n', '.---------------------------------\n', '.使用方法\n', '.- 输入功能前面的数字然后按回车\n', '.---------------------------------\n', '.功能列表:\n', '.1\t开关重锤\n', '1$assets\\minecraft\\models\\item\\mace.json\n', '.2\t开关泥土类\n', '2$assets\\minecraft\\blockstates\\podzol.json\n', '^assets\\minecraft\\blockstates\\dirt_path.json\n', '^assets\\minecraft\\blockstates\\grass_block.json\n', '^assets\\minecraft\\blockstates\\mycelium.json\n', '.3\t开关枯木白骨\n', '3$assets\\minecraft\\blockstates\\dead_bush.json\n', '^assets\\minecraft\\models\\block\\dead_bush1.json\n', '^assets\\minecraft\\models\\block\\dead_bush2.json\n', '^assets\\minecraft\\models\\block\\dead_bush3.json\n', '.---------------------------------\n', '.选择功能:'])
+        f.writelines(['\\\\\t\t    使用说明\n', '\\\\\n', '\\\\\t"\\\\"开始为注释(为啥配置文件要注释?)\n', '\\\\\t"."开始为直接输出，最后一个"."为读取输入\n', '\\\\\t类似"input$file"的内容为声明操作\n', '\\\\\t当输入"$"前面的内容时操作"$"后面的文件\n', '\\\\\t"$"的内容下面的"^"为同时操作的文件\n', '\\\\\t\t哦对了, 还有op~~~\n', '\n', '\n', '.禁用助手 Beta 2.1.2 - LIB临时工作室出品\n', '.---------------------------------\n', '.使用方法\n', '.- 输入功能前面的数字然后按回车\n', '.---------------------------------\n', '.功能列表:\n', '.1\t开关重锤\n', '1$assets\\minecraft\\models\\item\\mace.json\n', '\n', '.2\t开关泥土类\n', '2$assets\\minecraft\\blockstates\\podzol.json\n', '^assets\\minecraft\\blockstates\\dirt_path.json\n', '^assets\\minecraft\\blockstates\\grass_block.json\n', '^assets\\minecraft\\blockstates\\mycelium.json\n', '\n', '.3\t开关枯木白骨\n', '3$assets\\minecraft\\blockstates\\dead_bush.json\n', '^assets\\minecraft\\models\\block\\dead_bush1.json\n', '^assets\\minecraft\\models\\block\\dead_bush2.json\n', '^assets\\minecraft\\models\\block\\dead_bush3.json\n', '.---------------------------------\n', '.选择功能:'])
 except FileExistsError:
     pass
 
